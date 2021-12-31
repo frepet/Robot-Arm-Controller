89ed58f0c4a35a10fae62d9abe29cd8d0c981c2e
Fredrik Peteri Harr <fredrik@peteri.se>
2021-12-31 22:20:31 +0100

    Axex sliders generated in JS

--- robot-arm-controller.js
+++ robot-arm-controller.js
@@ -1,8 +1,32 @@
 const controllers = {};
+const sliders = [];
 
 function addGamepad(gamepad) {
   controllers[gamepad.index] = gamepad;
   document.getElementById("start").remove();
+
+  let prototype =
+  `<div class="axis-slider">
+    <label>X</label>
+    <input type="range" min="-1" max="1" value="0" step="0.01">
+  </div>`
+
+  for (let i = 0; i < gamepad.axes.length; i++) {
+    const node = document.createElement("div");
+    node.className = "axis-slider";
+    const label = document.createElement("label");
+    label.textContent = i.toString();
+    node.appendChild(label);
+    const slider = document.createElement("input");
+    slider.type = "range";
+    slider.min = "-1";
+    slider.max = "1";
+    slider.value = "0";
+    slider.step = "0.01";
+    sliders.push(node.appendChild(slider));
+    document.getElementById("axes").appendChild(node);
+  }
+
   window.requestAnimationFrame(updateStatus);
 }
 
@@ -22,14 +46,16 @@ function updateStatus() {
     for (let i=0; i<controller.buttons.length; i++) {
       let val = controller.buttons[i];
       context.fillStyle = val.pressed ? "red" : "gray";
-      context.fillRect(24*i, 0, 16, 16)
+      context.fillRect(24*i, 0, 16, 16);
 
-      context.fillStyle = "black"
-      context.fillText(i.toString(), 24*i + 4, 12, 16)
+      context.fillStyle = "black";
+      context.font = "14px sans";
+      context.textAlign = "center";
+      context.fillText(i.toString(), 24*i + 8, 14, 36);
     }
 
     for (let i=0; i<controller.axes.length; i++) {
-      context.fillText(controller.axes[i].toFixed(2), 10, 32 + 16*i, 500);
+      sliders[i].value = controller.axes[i];
     }
   }
   window.requestAnimationFrame(updateStatus);
